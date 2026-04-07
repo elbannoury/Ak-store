4764e1d2c17de7a46dcf4ad69981f64246b34bfd
elbannoury <elbannoury@gmail.com>
2026-04-07 09:01:14 +0100

    Change supabaseAnonKey to a new value
    Updated supabaseAnonKey to use a new value.

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'https://xbidzbisccuingzaunvv.supabase.co';
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'sb_publishable_KUIdove7-VvIHlKhA7wVZw_CAtd06hQ';
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhiaWR6YmlzY2N1aW5nemF1bnZ2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU1NDc1NTksImV4cCI6MjA5MTEyMzU1OX0.xh4f0-SbfDAymMvcIOWfeHPV_sWyl6XKLydmKBbUIG8';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
