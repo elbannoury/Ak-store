aeee7e050b91007bf5222c380f89ad8372a2e7a7
elbannoury <elbannoury@gmail.com>
2026-04-10 01:22:28 +0100

    Add vite-env.d.ts for Vite environment types

---
+++ src/vite-env.d.ts
@@ -0,0 +1,13 @@
+/// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_SUPABASE_URL: string
+  readonly VITE_SUPABASE_ANON_KEY: string
+  readonly VITE_WHATSAPP_NUMBER: string
+  readonly VITE_CALLMEBOT_API_KEY: string
+  readonly VITE_GOOGLE_SHEETS_URL: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
